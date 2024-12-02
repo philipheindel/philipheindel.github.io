3b0a9e6f539f7f8760ee60a72cf3d250782564e7
Philip Heindel <p.e.heindel@gmail.com>
2024-12-01 20:59:42 -0500

    Added date dropdown

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /**
+ * An array constant with an ordered list of the 12 months. 
+ */
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const DATE = new Date();
+
+/**
  * 
  * @param {string} selector 
  * @returns {Element}
@@ -16,6 +23,33 @@ function __(selector) {
     return document.querySelectorAll(selector);
 }
 
-_("#clicker").addEventListener("click", function() {
-    _("#header").innerText = "clicked but with get fn"
+/**
+ * Returns either the number, full name, or shortened name of the current month.
+ * 
+ * @param {string} type
+ * @returns {string}
+ */
+function getCurrentMonth() {
+    const d = new Date();
+    console.log(MONTHS[d.getMonth()]);
+}
+
+/**
+ * Returns an array 
+ * 
+ * @param {string} month 
+ * @returns {number}
+ */
+function getMonthDays(month) {
+    
+}
+
+_("#start").addEventListener("click", function() {
+    getCurrentMonth();
+    //_("#header").innerText = "clicked but with get fn"
+});
+var val = 0
+_("#month").addEventListener("change", function() {
+    console.log("changed " + val + " times");
+    val += 1;
 });
