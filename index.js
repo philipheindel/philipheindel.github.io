ec918664a33518fb24a2c53a220073f5a0f0aca8
Philip Heindel <p.e.heindel@gmail.com>
2024-12-04 14:39:08 +0000

    Some more updates and a bit of organization

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const MONTHS = ["January", "February", "March", "April", "May", "June", "July", 
 
 const DATE = new Date();
 
+//#region SELECTORS
 /**
  * 
  * @param {string} selector 
@@ -22,7 +23,9 @@ function _(selector) {
 function __(selector) {
     return document.querySelectorAll(selector);
 }
+//#endregion SELECTORS
 
+//#region DATE FUNCTIONS
 /**
  * Returns either the number, full name, or shortened name of the current month.
  * 
@@ -44,9 +47,29 @@ function getMonthDays(year, month) {
     return new Date(year, month, 0).getDate();
 }
 
+function getFirstDayOfMonth(year, month) {
+    new Date(2024, 1, 1).toLocaleDateString("en", { weekday: 'long' }); 
+}
+//#endregion DATE FUNCTIONS
+
+//#region HELPER FUNCTIONS
+function removeAlphaChars(elementId) {
+    _(elementId).value = _(elementId).value.replace(/\D/g,'');
+}
+//#endregion HELPER FUNCTIONS
+
+//#region INITIALIZATION FUNCTIONS
+function init() {
+    _("#year").defaultValue = "2024";
+}
+//#endregion INITIALIZATION FUNCTIONS
+
+//#region EVENT LISTENERS
 _("#start").addEventListener("click", function() {
+    
+    
     getCurrentMonth();
-    //_("#header").innerText = "clicked but with get fn"
+
 });
 
 _("#month").addEventListener("change", function(e) { 
@@ -56,6 +79,13 @@ _("#month").addEventListener("change", function(e) {
     console.log("There are " + getMonthDays(2024, monthNumber, 0) + " days in " + monthName + " of " + yearNumber);
 });
 
-_("#year").addEventListener("keypress", function(e) {
-    console.log("key pressed on " + this.value)
+_("#year").addEventListener("keyup", function(e) {
+    removeAlphaChars("#year");
 });
+
+_("#year").addEventListener("keydown", function(e) {
+    if (isNaN(e.key)) {
+        removeAlphaChars("#year");
+    }
+});
+//#endregion EVENT LISTENERS
